2ac12b8bbe1e28e01a5ca5bceea84a1b087a3f07
jacobbuckles-hc <jacob.buckles@hashicorp.com>
2023-09-05 20:59:45 -0500

    update input variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,14 @@
 variable "cidr_vpc" {
   description = "CIDR block for the VPC"
-  default     = "10.1.0.0/16"
+  type = string
 }
 
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
-  default     = "10.1.0.0/24"
+  type = string
 }
 
 variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
-  default     = {}
 }
